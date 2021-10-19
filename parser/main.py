a166fc17ceccd0857cc942ef8b469cda910e6f82
anastasia-spb <anastasia.vlasova6293@gmail.com>
2021-10-19 23:32:09 +0300

    Add info

--- parser/main.py
+++ parser/main.py
@@ -5,10 +5,10 @@ from parses import parse_tags, parse_questions
 # Every page contains 50 questions.
 # @ref QUESTION_PAGES * 50 records will be
 # in tag csv file
-QUESTION_PAGES = 1
+QUESTION_PAGES = 500
 # Each page with tags contains 36 tags.
 # For every tag @ref QUESTION_PAGES will be parsed
-TAGS_PAGES = 1
+TAGS_PAGES = 100
 # Directory to which output csv files will be generated
 DATA_DIRECTORY = 'data_' + str(uuid.uuid4())
 
@@ -25,10 +25,12 @@ if __name__ == '__main__':
         os.makedirs(data_directory)
 
     for page in range(1, TAGS_PAGES + 1):
+        print("Start parsing page {}/{} of tags".format(page, TAGS_PAGES))
         tags_df = parse_tags(page)
         tags_df.to_csv(f'{data_directory}/all_tags.csv', index=True, sep=':', mode='a')
 
         for current_tag in tags_df['tags']:
+            print("Start parsing {} tag".format(current_tag))
             write_to_csv_questions_for_tag(current_tag, data_directory)
 
     print("Data generation is finished, check {} repository".format(data_directory))
